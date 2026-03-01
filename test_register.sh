823dd8725eedf7ffda4861d74f2e3a6d1555182e
ali25developer <ali25developer@gmail.com>
2026-03-01 13:04:59 +0700

    chore: remove test script

--- test_register.sh
+++
@@ -1,9 +0,0 @@
-curl -X POST \
-  http://localhost:8000/api/v1/auth/register \
-  -H 'Content-Type: application/json' \
-  -d '{
-    "name": "Test User",
-    "email": "test@example.com",
-    "password": "password123",
-    "phone": "08123456789"
-}'
